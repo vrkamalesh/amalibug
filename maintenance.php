f22b93f328cf684479a4914e39535d365bfe5e4e
vrkamalesh <vrkamalesh13@gmail.com>
2023-05-10 11:49:42 +0800

    first commit

---
+++ maintenance.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>convert lenght</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div id="main-content">
+        <h1 style="text-align:center;">OoopSs! sorry .</h1>
+          <img src="maintenance.jpg" alt="">
+          <p style= "text-align:center;">Return to <a href="index.php">main menu</a></p>
+
+
+    </div>
+    
+</body>
+</html>
